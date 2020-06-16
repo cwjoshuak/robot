d84a013dbcbddb0eb57304ee6f2ed1d54e1be29b
Joshua <jckuan1@uci.edu>
2020-06-16 01:39:52 -0700

    initial bot with course info command

--- bot.py
+++ bot.py
@@ -1,4 +1,19 @@
 import discord
+import os
+
+from course import Course
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+MONGO_PW = os.getenv("MONGO_PW")
+
+DISCORD_API_KEY = os.getenv("DISCORD_API_KEY")
+
+client = MongoClient(f"mongodb+srv://root:{MONGO_PW}@course-data-f6kx1.mongodb.net/coursedb?retryWrites=true&w=majority")
+db = client.coursedb
+mongo_courses = db.courses
 
 client = discord.Client()
 
@@ -11,7 +26,41 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('.ping'):
+        await message.channel.send('Pong!')
 
-client.run('')
+    if message.content.startswith('.cinfo'):
+        whole_course = [i.upper() for i in message.content.split(' ') if i != '']
+
+        if 3 <= len(whole_course) <= 4:
+            dept_name = whole_course[1]
+            course_number = whole_course[2]
+
+            if len(whole_course) == 4:
+                dept_name = ' '.join(whole_course[1:3])
+                course_number = whole_course[3]
+
+            course_dict = mongo_courses.find_one({'_id': dept_name})
+            if course_dict is None:
+                alias = db.aliases.find_one({'_id': dept_name})
+                if alias is not None:
+                    dept_name = alias['original']
+                    course_dict = mongo_courses.find_one({'_id': dept_name})
+
+            if course_dict is not None and course_number in course_dict:
+                course = Course.from_dict(dept_name, course_dict[course_number])
+
+                embed = course.to_embed()
+                
+                await message.channel.send(embed=embed)
+        
+    if message.content.startswith('.addalias'):
+        aliases = db.aliases
+        contents = message.content.split(' ')
+        result = aliases.insert_one({'_id': contents[1].upper(), 'original': ' '.join(contents[2:]).upper()})
+        await message.channel.send(f'Aliased {' '.join(contents[2:]).upper()} as {result.inserted_id}')
+
+    if message.content.startswith('.listaliases'):
+        aliases = db.aliases
+        await message.channel.send('```' + ''.join(str(list(aliases.find()))) + '```')
+client.run(DISCORD_API_KEY)
---
+++ course.py
@@ -0,0 +1,60 @@
+from discord import Embed
+
+class Course:
+    def __init__(self, dept_code, course_number, course_title, num_units, course_desc, requisite, grading, repeatability, restriction, ge_req, similar, overlaps):
+        self.dept_code = dept_code
+        self.course_number = course_number
+        self.course_title = course_title
+        self.num_units = num_units
+        self.course_desc = course_desc
+
+        self.requisite = requisite
+        self.grading = grading
+        self.repeatability = repeatability
+        self.restriction = restriction
+        self.ge_req = ge_req
+        self.similar = similar
+        self.overlaps = overlaps
+
+
+    @classmethod
+    def from_dict(cls, dept_code, a_dict):
+        return cls(dept_code, a_dict['course_number'], a_dict['course_title'], a_dict['num_units'], 
+        a_dict['course_desc'], a_dict['requisite'], a_dict['grading'], a_dict['repeatability'], a_dict['restriction'], a_dict['ge_req'], a_dict['similar'], a_dict['overlaps'])
+
+    def to_embed(self):
+        embed = Embed(title=f'{self.course_title}', description=f'{self.dept_code} {self.course_number} - *{self.num_units}*', color=0x879BAF)
+        if self.course_desc != '':
+            embed.add_field(name="Course Description", value=self.course_desc, inline=False)
+        if self.requisite != '':
+            embed.add_field(name="Course Requisites", value=self.requisite, inline=False)
+       
+        if self.restriction != '':
+            embed.add_field(name="Course Restrictions", value=self.restriction, inline=False)
+       
+        if self.similar != '':
+            embed.add_field(name="Similar Courses", value=self.similar, inline=False)
+        if self.overlaps != '':
+            embed.add_field(name="Course Overlaps", value=self.overlaps, inline=False)
+        if self.grading != '':
+            embed.add_field(name="Grading Options", value=self.grading, inline=False)
+        if self.repeatability != '':
+            embed.add_field(name="Course Repeatability", value=self.repeatability, inline=False)
+        if self.ge_req != '':
+            embed.add_field(name="Satifies GE Requirement", value=self.ge_req, inline=False)
+        return embed
+    def __str__(self):
+        return \
+f'''
+{self.dept_code} {self.course_number}
+{self.course_title} - {self.num_units}
+{self.course_desc}
+
+{self.requisite}
+{self.grading}
+{self.repeatability}
+{self.restriction}
+{self.ge_req}
+{self.similar}
+{self.overlaps}
+'''
